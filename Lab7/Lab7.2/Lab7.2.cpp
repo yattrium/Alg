91ab65b03320b45491c593d25e59e37dc9b95da6
yattrium <wolf.mikoto@gmail.com>
2019-12-22 20:40:35 +0300

    честно? я хочу умереть

---
+++ Lab7/Lab7.2/Lab7.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int main() 
+{
+	system("color F0");
+	setlocale(LC_ALL, "Russian");
+	int k;
+	string s;
+	cout << "Введите строку: ";
+	getline(cin, s);
+	for (int i = 0; i < s.length(); i++) {
+		s[i] = tolower(s[i]);
+	}
+	cout << "Преобразованная строка: " << s << "\n";
+	system("pause");
+	return 0;
+}
---
+++ Lab7/Lab7.3/Lab7.3.cpp
@@ -0,0 +1,31 @@
+﻿//#include "pch.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("color F0");
+	setlocale(LC_ALL, "Russian");
+	string str1, str2;
+	cout << "Введите первую строку: ";
+	getline(cin, str1);
+	cout << "Введите вторую строку: ";
+	getline(cin, str2);
+
+	if (str1.length() == str2.length())
+	{
+		for (int i = 0; i < str1.length(); i++)
+		{
+			if (tolower(str1[i]) != tolower(str2[i]))
+			{
+				cout << endl << "no"<<"\n";
+				return 0;
+			}
+		}
+		cout << endl << "yes" << "\n";
+	}
+	else cout << endl << "no" << "\n";
+	system("pause");
+	return 0;
+}
